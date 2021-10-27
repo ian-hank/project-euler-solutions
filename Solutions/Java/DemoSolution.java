cdba17361332fb46e6576e073b204eadd8ea63c0
ian-hank <57643516+ian-hank@users.noreply.github.com>
2021-10-27 14:51:37 -0400

    Problem 3 and SolutionTesting Fix
    -Finished P3 and fixed the output from SolutionTesting printf statement

---
+++ Solutions/Java/DemoSolution.java
@@ -0,0 +1,30 @@
+/* 
+BACKGROUND
+background info goes here
+
+QUESTION
+main question goes here
+*/
+
+public class DemoSolution extends SolutionTesting {
+    //The actual solution to the problem
+    final static int ACTUAL_ANSWER = 000000;
+    public static void main(String[] args) {
+        EnsureSolution(Solution1(), ACTUAL_ANSWER, 1);
+    }
+
+    //SOLUTIONS
+    public static long[] Solution1() {
+        long[] result = new long[2];
+        long startTime = System.nanoTime();
+
+        //START SOLUTION
+        
+        //END SOLUTION
+
+        long endTime = System.nanoTime();
+        //Answer goes in result[0]
+        result[1] = endTime - startTime;
+        return result;
+    }
+}
---
+++ Solutions/Java/P3.java
@@ -0,0 +1,40 @@
+/* 
+BACKGROUND
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+QUESTION
+What is the largest prime factor of the number 600851475143?
+*/
+
+import java.util.ArrayList;
+
+public class P3 extends SolutionTesting {
+    //The actual solution to the problem
+    final static int ACTUAL_ANSWER = 6857;
+    public static void main(String[] args) {
+        EnsureSolution(Solution1(), ACTUAL_ANSWER, 1);
+    }
+
+    //SOLUTIONS
+    public static long[] Solution1() {
+        long[] result = new long[2];
+        long startTime = System.nanoTime();
+
+        //START SOLUTION
+        ArrayList<Long> numList = new ArrayList<Long>();
+        long number = 600_851_475_143L; 
+        
+        for (long i = 1L; i <= number; i++) {
+            if (number % i == 0) {
+                numList.add(i);
+                number /= i;
+            }
+        }
+        //END SOLUTION
+
+        long endTime = System.nanoTime();
+        result[0] = (long) numList.get( numList.size() - 1);
+        result[1] = endTime - startTime;
+        return result;
+    }
+}
