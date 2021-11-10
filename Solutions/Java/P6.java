da0caff41405e2f8ebd801651fb3c949017e39f8
ian-hank <57643516+ian-hank@users.noreply.github.com>
2021-11-09 20:20:10 -0500

    Finishing P6 and P7
    P6 - Completed Problem
    
    P7 - Completed Problem
    
    P8 - Have it laid out to solve for the first time

---
+++ Solutions/Java/P6.java
@@ -0,0 +1,45 @@
+/* 
+BACKGROUND
+The sum of the squares of the first ten natural numbers is, 
+1^2 + 2^2 +...+ 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 +...+ 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+3025 - 385 = 2640
+
+QUESTION
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+public class P6 extends SolutionTesting {
+    //The actual solution to the problem
+    final static int ACTUAL_ANSWER = 25164150;
+    public static void main(String[] args) {
+        EnsureSolution(Solution1(), ACTUAL_ANSWER, 1);
+    }
+
+    //SOLUTIONS
+    public static long[] Solution1() {
+        long[] result = new long[2];
+        long startTime = System.nanoTime();
+
+        //START SOLUTION
+        int squareSum = 0;
+        int sumSquare = 0;
+        int squareDifference = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            squareSum += Math.pow(i, 2);
+            sumSquare += i;
+        }
+        squareDifference = (int)Math.pow(sumSquare, 2) - (squareSum);
+        //END SOLUTION
+
+        long endTime = System.nanoTime();
+        result[0] = squareDifference;
+        result[1] = endTime - startTime;
+        return result;
+    }
+}
---
+++ Solutions/Java/P7.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+/* 
+BACKGROUND
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+QUESTION
+What is the 10 001st prime number?
+*/
+
+public class P7 extends SolutionTesting {
+    //The actual solution to the problem
+    final static int ACTUAL_ANSWER = 104743;
+    public static void main(String[] args) {
+        EnsureSolution(Solution1(), ACTUAL_ANSWER, 1);
+    }
+
+    //SOLUTIONS
+    public static long[] Solution1() {
+        long[] result = new long[2];
+        long startTime = System.nanoTime();
+
+        //START SOLUTION
+        ArrayList<Integer> primeList = new ArrayList<Integer>();
+        for (int i = 1; primeList.size() != 10_001; i++) {
+
+               int counter = 0;
+               for (int num = i; num >= 1; num--) {
+                   if (i % num == 0) {
+                    counter += 1;
+                   }
+               }
+               if (counter == 2) {
+                primeList.add(i);
+            }
+        }
+        System.out.println(primeList.toString());
+        //END SOLUTION
+
+        long endTime = System.nanoTime();
+        result[0] = primeList.get(10_001 - 1);
+        result[1] = endTime - startTime;
+        return result;
+    }
+}
